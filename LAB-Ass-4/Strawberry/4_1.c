944fa01855db5525a2e5389517dae441d73e42bf
Nayeem Mahmood <com.debugster@gmail.com>
2020-03-10 01:15:45 +0600

    Create 4_1.c

---
+++ LAB-Ass-4/Strawberry/4_1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int dec, dec_copy, i, j, bin[200];
+    printf("Enter a non-negative decimal number : ");
+    scanf("%d", &dec);
+    dec_copy = dec;
+    i = 0;
+    do {
+        bin[i] = dec_copy % 2;
+        dec_copy = dec_copy / 2;
+        i++;
+    } while (dec_copy != 0);
+
+    printf("Decimal\t: %d\nBinary\t: ", dec);
+    for (j = i - 1; j >= 0; j--) {
+        printf("%d", bin[j]);
+    }
+    return 0;
+}
